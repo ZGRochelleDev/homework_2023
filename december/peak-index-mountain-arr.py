f5cb502e913133f5388c7196541b7b1c7060337d
ZGRochelle <zgrochelle@gmail.com>
2023-12-15 11:39:09 -0500

    12-15-2023

--- december/peak-index-mountain-arr.py
+++ december/peak-index-mountain-arr.py
@@ -1,3 +1,5 @@
+## difficult to find efficient solution
+
 #https://leetcode.com/problems/peak-index-in-a-mountain-array/submissions/
 class Solution:
     def peakIndexInMountainArray(self, arr):
@@ -25,3 +27,30 @@ arr = [3,4,5,1]
 s = Solution()
 ans = s.peakIndexInMountainArray(arr)
 print(f"peakIndexInMountainArray -> {ans}")
+
+
+class Solution:
+
+    def binary_iter(self, arr):
+
+        return
+
+    ## implement this from scratch
+    def binary(self, left, right, arr):
+        mid = int((left + right) / 2)
+        if arr[mid-1] < arr[mid] and arr[mid] > arr[mid+1]:
+            return mid
+        elif arr[mid-1] < arr[mid]:
+            return self.binary(mid + 1, right, arr)
+        else:
+            return self.binary(left, mid, arr)
+
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        # i = 1
+        # while i < len(arr)-1:
+        #     if arr[i-1] < arr[i] and arr[i] > arr[i+1]:
+        #         return i
+        #     else:
+        #         i+=1
+
+        return self.binary(0, len(arr)-1, arr)
--- december/range-sum-query.py
+++ december/range-sum-query.py
@@ -1,4 +1,5 @@
 #https://leetcode.com/problems/range-sum-query-immutable/description/
+## Attempt 2 ##
 class NumArray:
     def __init__(self, nums):
         self.sum_dict = {}
@@ -11,7 +12,27 @@ class NumArray:
             i+=1
 
     def sumRange(self, left: int, right: int):
-      return self.sum_dict[left] - self.sum_dict[right]
+      if left == 0:
+          return self.sum_dict[right]
+      else:
+        return self.sum_dict[right]-self.sum_dict[left-1]
+
+## Attempt 1 ##
+# class NumArray:
+#     nums_list = []
+#     arr_sum = []
+
+#     def __init__(self, nums: List[int]):
+#         self.nums_list = nums
+#         ## add preprocessing here
+#         ## to call in sumRange
+
+#     def sumRange(self, left: int, right: int) -> int:
+      
+#       sum = 0
+#       for pos in range(left, right+1):
+#           sum = sum + self.nums_list[pos]
+#       return sum
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
@@ -20,13 +41,15 @@ class NumArray:
 # Input
 # ["NumArray", "sumRange", "sumRange", "sumRange"]
 # [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
-
+#              2          5
 nums = [-2, 0, 3, -5, 2, -1]
 params = [[0, 2], [2, 5], [0, 5]]
 NA = NumArray(nums)
+print(NA.sum_dict.values())
+ans = []
 for sub_arr in params:
-    ans = NA.sumRange(sub_arr[0], sub_arr[1])
-    print(ans)
+    ans.append(NA.sumRange(sub_arr[0], sub_arr[1]))
+print(f"ans -> {ans}")
 
 # Expected Output
 # [1,-1,-3]
@@ -37,4 +60,43 @@ for sub_arr in params:
 # [-2, 0, 3, -5, 2, -1]
 #  -2 -2  1  -4 -2  -3
 
-# nums_dict = {}
+
+
+class NumArray:
+    def __init__(self, nums):
+        self.nums = nums
+        self.sum_dict = nums # array better space?
+
+        i = 0
+        while i < len(nums):
+            if i == 0:
+                self.sum_dict[i] = nums[i]
+            else:
+                self.sum_dict[i] += self.sum_dict[i-1]
+            i+=1
+
+    def sumRange(self, left: int, right: int):
+      if left == 0:
+          return self.sum_dict[right]
+      else:
+        return self.sum_dict[right] - self.sum_dict[left-1]
+
+# class NumArray:
+#     nums_list = []
+#     arr_sum = []
+
+#     def __init__(self, nums: List[int]):
+#         self.nums_list = nums
+#         ## add preprocessing here
+#         ## to call in sumRange
+
+#     def sumRange(self, left: int, right: int) -> int:
+      
+#       sum = 0
+#       for pos in range(left, right+1):
+#           sum = sum + self.nums_list[pos]
+#       return sum
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
