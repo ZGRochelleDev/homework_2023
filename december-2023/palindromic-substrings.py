df0f2995810bd2070656a2ccd2ada2d6e2f05405
ZGRochelle <zgrochelle@gmail.com>
2024-01-10 11:25:32 -0500

    Update palindromic-substrings.py

--- december-2023/palindromic-substrings.py
+++ december-2023/palindromic-substrings.py
@@ -46,16 +46,17 @@ class Solution:
         return no_of_substrings
 
     def countSubstrings_2(self, s):
-        no_of_substrings = 0
+        substrings_count = 0
         str_size = len(s)
         for i in range(str_size):
             for a, b in [(i, i), (i, i+1)]:
-                print(a,b,"-",[(i, i), (i, i+1)])
+                #print(a,b,"-",[(i, i), (i, i+1)])
                 while a >= 0 and b < str_size and s[a] == s[b]:
                     a -= 1
                     b += 1
-                no_of_substrings += (b-a)//2
-        return no_of_substrings
+                substrings_count += (b-a)//2
+        return substrings_count
+
 
 
 str = "aa"
@@ -66,4 +67,3 @@ ans = s.countSubstrings_2(str)
 #ans = s.countSubstrings(str)
 print("palindromic-substrings")
 print(f"answer -> {ans}")
-
